68647eeefb5721703b0bcf0469661a27187acd4c
golanha <golanha@rnd-hub.com>
2020-11-29 11:36:58 +0200

    build by npm

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ sleep 20
 export javaWrapperVersion='1.3.04'
 envsubst < ${SCRIPTPATH}/m2-project/pomTemplate.xml >  ${SCRIPTPATH}/m2-project/pom.xml
 docker  run --network="host" --rm  -v $SCRIPTPATH/m2-project:/m2-project -w /m2-project maven mvn --settings /m2-project/settings.xml package  
-docker container commit archiva hkube/maven-registry:${javaWrapperVersion}
-docker push  hkube/maven-registry:${javaWrapperVersion}
+docker container commit archiva hkube/maven-registry:${npm_package_version}
+docker push  hkube/maven-registry:${npm_package_version}
 
