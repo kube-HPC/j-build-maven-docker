bba63c739b7e3d91a523b8a27e308315ada6a914
golanha <golanha@rnd-hub.com>
2020-11-29 12:52:46 +0200

    2-snpashot

--- build.sh
+++ build.sh
@@ -4,9 +4,10 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 docker stop archiva || true && docker rm archiva || true
 docker run --name archiva --network="host" -p  8080:8080 xetusoss/archiva &
 sleep 20
-export javaWrapperVersion='1.3.04'
+export javaWrapperVersion='2.0-SNAPSHOT'
 envsubst < ${SCRIPTPATH}/m2-project/pomTemplate.xml >  ${SCRIPTPATH}/m2-project/pom.xml
 docker  run --network="host" --rm  -v $SCRIPTPATH/m2-project:/m2-project -w /m2-project maven mvn --settings /m2-project/settings.xml package  
-docker container commit archiva hkube/maven-registry:${npm_package_version}
-docker push  hkube/maven-registry:v${npm_package_version}
+export VERSION=v${npm_package_version}
+docker container commit archiva hkube/maven-registry:${VERSION}
+docker push  hkube/maven-registry:$VERSION}
 
