951f13cbd42e5d754bad90f5c06c5a7cede1329c
golanha <golanha@rnd-hub.com>
2020-12-01 01:15:26 +0200

    alternative archiva-data path

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,7 @@
 from xetusoss/archiva
-COPY ./archiva-data /archiva-data
-ENTRYPOINT [ "/entrypoint.sh" ]
+USER archiva
+COPY ./archiva-data/ /archiva-data2/
+USER root
+RUN chown -R archiva:archiva /archiva-data2
+ENV ARCHIVA_BASE /archiva-data2
+USER archiva
--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 set -eo pipefail
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 docker stop archiva || true && docker rm archiva || true
-# docker run --name archiva --network="host" -p  8080:8080 hkube/empty-archiva:1.0 &
 docker run --name archiva --network="host" -p  8080:8080 -v /home/golanha/dev/hkube/maven.git/archiva-data/:/archiva-data xetusoss/archiva &
 sleep 20
 export javaWrapperVersion='2.0-SNAPSHOT'
