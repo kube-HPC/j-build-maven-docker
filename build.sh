79dd1a441ebd4f749dd76f6304bd8beaaff6e2b5
golanha <golanha@rnd-hub.com>
2020-12-06 10:56:04 +0200

    new nexus repository configuraion

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -eo pipefail
-# SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-# # docker stop nexus || true && docker rm nexus || true
-# docker run -d --rm -p 8081:8081 --name nexus -v ${SCRIPTPATH}/sonatype-work/:/sonatype-work sonatype/nexus
-# sleep 20
-# export javaWrapperVersion='2.0-SNAPSHOT'
-# envsubst < ${SCRIPTPATH}/m2-project/pomTemplate.xml >  ${SCRIPTPATH}/m2-project/pom.xml
-# docker  run --network="host" --rm  -v $SCRIPTPATH/m2-project:/m2-project -w /m2-project maven mvn --settings /m2-project/settings.xml package  
-# sudo rm -r -f ${SCRIPTPATH}/sonatype-work/.java
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+docker stop nexus || true && docker rm nexus || true
+docker run -d --rm -p 8081:8081 --name nexus -v ${SCRIPTPATH}/nexus-data/:/nexus-data   sonatype/nexus3
+sleep 40
+export javaWrapperVersion='2.0-SNAPSHOT'
+envsubst < ${SCRIPTPATH}/m2-project/pomTemplate.xml >  ${SCRIPTPATH}/m2-project/pom.xml
+docker  run --network="host" --rm  -v $SCRIPTPATH/m2-project:/m2-project -w /m2-project maven mvn --settings /m2-project/settings.xml package  
+sudo rm -r -f ${SCRIPTPATH}/sonatype-work/.java
 export VERSION=v${npm_package_version}
 docker build -f ./Dockerfile -t hkube/maven-registry:${VERSION} .
-# docker push  hkube/maven-registry:${VERSION}
+docker push  hkube/maven-registry:${VERSION}
 
