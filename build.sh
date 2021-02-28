e2a6999c481480eaf4fd7d3616e1b8df90fdfc00
golanha <golanha@rnd-hub.com>
2021-02-28 14:45:20 +0200

    java wrapper version

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ docker stop nexus || true && docker rm nexus || true
 chmod -R 777 ${SCRIPTPATH}/nexus-data || true
 docker run -d --rm -p 8081:8081 --name nexus -v ${SCRIPTPATH}/nexus-data/:/nexus-data   sonatype/nexus3
 sleep 50
-export javaWrapperVersion='1.3.05'
+export javaWrapperVersion='1.3.06'
 envsubst < ${SCRIPTPATH}/m2-project/pomTemplate.xml >  ${SCRIPTPATH}/m2-project/pom.xml
 docker  run --network="host" --rm  -v $SCRIPTPATH/m2-project:/m2-project -w /m2-project maven mvn --settings /m2-project/settings.xml package  
 docker  run --network="host" --rm  -v $SCRIPTPATH/m2-project:/m2-project -w /m2-project maven mvn dependency:get --settings /m2-project/settings.xml  -Dartifact=io.hkube:wrapper:${javaWrapperVersion}:jar:wide 
