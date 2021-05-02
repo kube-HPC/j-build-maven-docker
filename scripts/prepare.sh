95b6381af2535d602187ec9083f6c43391b62c30
Yehiyam Livneh <yehiyam@gmail.com>
2021-05-02 15:40:34 +0300

    nexus-data-template

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -5,7 +5,7 @@ NETWORK_NAME=artifacts-registry-network
 docker stop nexus || true && docker rm nexus || true
 docker network rm ${NETWORK_NAME} || true
 docker network create ${NETWORK_NAME}
-chmod -R 777 ${SCRIPTPATH}/../nexus-data || true
+cp -r nexus-data-template nexus-data
 docker run -d --network=${NETWORK_NAME} -p 8081:8081 --name nexus -v ${SCRIPTPATH}/../nexus-data/:/nexus-data   sonatype/nexus3
 echo Waiting for nexus to be up
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8081)" != "200" ]]; do echo -n .; sleep 5; done; echo ""
