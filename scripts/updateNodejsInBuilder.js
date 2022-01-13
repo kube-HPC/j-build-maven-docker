5ee1aeef8196f4d01fb15d9afd0aeea407efcf27
golanha <golanha@rnd-hub.com>
2022-01-13 14:26:38 +0200

    update nodejs wrapper in other packages

--- scripts/updateNodejsInBuilder.js
+++ scripts/updateNodejsInBuilder.js
@@ -8,18 +8,6 @@ const ownerRepo = {
 };
 
 const main = async () => {
-    const file = 'core/algorithm-builder/environments/nodejs/wrapper/package.json';
-    const octokit = new Octokit({ auth: process.env.GH_TOKEN });
-    const packageJsonContentResponse = await octokit.repos.getContent({
-        ...ownerRepo,
-        path: file
-    });
-    const packageJsonContentStr = Buffer.from(packageJsonContentResponse.data.content, 'base64').toString('utf-8');
-    const packageJsonContent = JSON.parse(packageJsonContentStr);
-    const packageJsonSha = packageJsonContentResponse.data.sha;
-    // update package json
-    packageJsonContent.dependencies['@hkube/nodejs-wrapper'] = `^${version}`;
-    const newContent = Buffer.from(JSON.stringify(packageJsonContent, null, 2)).toString('base64');
 
     const masterShaResponse = await octokit.repos.listCommits({
         ...ownerRepo,
@@ -32,15 +20,31 @@ const main = async () => {
         ref: `refs/heads/${branchName}`,
         sha: masterSha
     });
-    await octokit.repos.createOrUpdateFileContents({
-        ...ownerRepo,
-        path: file,
-        message: `update nodejs wrapper to ${version}`,
-        branch: branchName,
-        sha: packageJsonSha,
-        content: newContent
-    });
 
+    const files = process.env.updatedFilePath.split(";");
+    files.forEach(file => {
+        const octokit = new Octokit({ auth: process.env.GH_TOKEN });
+        const packageJsonContentResponse = await octokit.repos.getContent({
+            ...ownerRepo,
+            path: file
+        });
+        const packageJsonContentStr = Buffer.from(packageJsonContentResponse.data.content, 'base64').toString('utf-8');
+        const packageJsonContent = JSON.parse(packageJsonContentStr);
+        const packageJsonSha = packageJsonContentResponse.data.sha;
+        // update package json
+        packageJsonContent.dependencies['@hkube/nodejs-wrapper'] = `^${version}`;
+        const newContent = Buffer.from(JSON.stringify(packageJsonContent, null, 2)).toString('base64');
+
+
+        await octokit.repos.createOrUpdateFileContents({
+            ...ownerRepo,
+            path: file,
+            message: `update nodejs wrapper to ${version}`,
+            branch: branchName,
+            sha: packageJsonSha,
+            content: newContent
+        });
+    });
     await octokit.pulls.create({
         ...ownerRepo,
         title: `update nodejs wrapper to ${version}`,
